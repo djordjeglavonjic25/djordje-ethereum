c8551744588f4e0b220a215f3940ce645504564c
djordjeglavonjic25 <236400041+djordjeglavonjic25@users.noreply.github.com>
2026-07-25 12:01:16 +0200

    Azurirana auto_push skripta

--- auto_push.sh
+++ auto_push.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-while true; do
-  git add .
-  git commit -m "Auto-sync: $(date)" || true
-  git push origin japantokio
-  sleep 300
-done
+cd ~/djordje-ethereum
+python berze_integracija.py
+git add podaci/berze_status.json berze_integracija.py
+git commit -m "Automatsko azuriranje berzi i sinhronizacija"
+git push origin japantokio
